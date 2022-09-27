6478df7e6544c8b38ce4bb7ee64559be9cd768df
Shifali Singh <shifali@appsimpact.com>
2022-09-27 23:23:11 +0530

    added more questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,7 +54,54 @@
 # end
 
 
-question = Question.create(
-  title: 'what is rails?',
-  answer: 'rails is server-side web application framework written in ruby.'
-  )
+# question = Question.create(
+#   title: 'what is rails?',
+#   answer: 'rails is server-side web application framework written in ruby.'
+#   )
+
+# question = Question.find_or_create_by(title: 'What is Action Controller?',
+#   answer: 'Action Controller 
+
+# C in MVC Architecture
+# C mediater between model and view ..it makes model data available view to display for the browser .
+
+
+# Client - Request - Server - Router - Controller- Output
+
+# Naming convention - plural (but not always ..prefer to be plural)
+
+# Inherit to application controller and application controller inherit to action controller:: Base
+
+# action  (show and index)
+
+# we can not use private method as action in controller because it is private and protected.
+
+# Controller allows callbacks ')
+
+
+question = Question.find_or_create_by(title: 'What is database.yml?', answer: 'Host-URL of the Host Machine where database is installed. If over a network then IP/Web address and if local then 127.0.0.1.Database-The name of database to be selected. Rails way is mydb_development.Username-The username of the database chosen.Password-password for the user account with given username to the database.Adaptor-Type of database being used for the Rails application (can be any MySQL/Oracle/Sqlite... etc).Pooling-Related to maximum number of connections to be made with database.')
+
+question = Question.find_or_create_by(title: 'What is Active Record Model?', answer: '-Active Record represents the model in MVC architecture.
+-It is responsible for representing business data and logic(associated with any real world entity eg - student , and all student as entities)
+-It allows you to create and use the business objects whose data requires persistent storage to a database
+-Model use ORM framework to store objects into database.
+
+ORM -Object Relation Mapping framework
+
+Object Relational Mapping is the abbreviation for the ORM framework. It maps the data between the database and object-oriented program. ORM framework is written in an object-oriented language and is also virtually wrapped around the relational database. There are many ORM frameworks.
+
+sql - select * from students;
+ORM - Student.all (simple and easy)
+
+Naming Convention - model name is always singular and table name is always plural
+
+and migration also generated for same model
+
+we can CRUD operations (create , read , update and destroy)
+
+model supports validation, callback, migrations 
+')
+
+question = Question.find_or_create_by(title: 'What is DRY principle? Explain.', answer: 'Pending...')
+
+question = Question.find_or_create_by(title: 'What is Convention over configuration?', answer: 'Pending...')
